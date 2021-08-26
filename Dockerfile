0ccaf3870c5f09fe540f7959d6a64f49910bee4b
slafniy <slafniy@gmail.com>
2021-08-26 16:11:17 +0400

    Works somehow with selenium

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 # syntax=docker/dockerfile:1
 
-FROM python:3.9.2
+FROM centos:latest
+
+RUN dnf install python39 firefox -y
+
 WORKDIR /app
 COPY . .
 RUN pip3 install -r requirements.txt
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import time
 import sys
 
 import telebot
 
+from img_search_selenium import ImageSearcher
 
 if __name__ == '__main__':
     token = sys.argv[1]
     print(f'Connecting with token {token}')
     bot = telebot.TeleBot(token=token)
 
-    @bot.message_handler()
+    searcher = ImageSearcher()
+
+
+    @bot.message_handler(commands=['img'])
     def echo_message(msg: telebot.types.Message):
-        bot.send_message(msg.chat.id, f'You said {msg.text}')
-
-
+        end = time.time() + 10
+        while time.time() < end:
+            try:
+                bot.send_photo(msg.chat.id, searcher.search_image(msg.text), reply_to_message_id=msg.id)
+                break
+            except Exception as e:
+                bot.send_message(msg.chat.id, f'Shit happened: {e}', reply_to_message_id=msg.id)
     bot.polling()
